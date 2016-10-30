3bbaae7706e6424f098d081af8ddb5e64cdb517f
Catherine <catherine.bacon@gmail.com>
2016-10-30 15:35:25 +0000

    store search terms in mongo and only return last 10

--- server.js
+++ server.js
@@ -9,22 +9,6 @@ var mongo = require('mongodb').MongoClient;
 var imageSearch = require('node-google-image-search');
 
 
-mongo.connect(mongoURL, function(err, db) {
-    if (err) throw err;
-    db.collection('searchlist').count( {}, function(error, count) {
-        if (count == 0) {
-            console.log('bum');
-            db.collection('searchlist').insert(
-                {
-                    _id: 1,
-                    list: []
-                }
-            );
-        }
-        db.close();    
-    });
-    
-});
 
 
 //Add entry to mongo database
@@ -32,9 +16,8 @@ var recordSearch = function(term) {
     mongo.connect(mongoURL, function(err, db) {
         if (err) throw err;
         var collection = db.collection('searchlist');
-        collection.update(
-            {_id: 1},
-            { $push: { list: {search: term, date: new Date()}  } }
+        collection.insert(
+            {search: term, date: new Date()}
         );
         db.close();
     });    
@@ -76,18 +59,16 @@ app.get('/latest/imagesearch', function(req, res) {
     mongo.connect(mongoURL, function(err, db) {
         if (err) throw err;
         var collection = db.collection('searchlist');
-        collection.find({
-            _id: 1
-        }).toArray( function(err, data) {
-            if (err) throw err;
-            res.json(data[0].list);
+        collection.find({}, {_id: 0})
+            .sort({ date: -1 })
+            .limit(10)
+            .toArray( function(err, data) {
+                if (err) throw err;
+                res.json(data);
         });
-        db.close();
+        //db.close();
     });
-
-    // return json {'term': 'searchterm', 'when': 'dateofsearch'}
 });
-
 
 
 
